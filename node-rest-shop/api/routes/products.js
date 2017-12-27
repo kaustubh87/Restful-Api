9fb109b349d9385a1d3f89dd90f35d04b6cff9cf
kvin1087 <kaustubh87@gmail.com>
2017-12-27 05:41:14 -0600

    Added Patch and Delete Routes

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -10,7 +10,41 @@ router.get('/', (req,res,next) => {
 router.post('/', (req,res,next) => {
     res.status(200).json({
         message: 'Handling POST requests to /products'
+       
     });
+});
+
+router.get('/:productId', (req,res,next) => {
+    const pId = req.params.productId;
+    if(pId == 'special'){
+        res.status(200).json({
+            message: 'You discovered the special Id',
+            pId: pId
+        });
+    }
+    else{
+        res.status(200).json({
+            message: 'You passed an Id'
+        })
+
+    }
+
+});
+
+router.patch('/:productId', (req,res,next) => {
+    
+        res.status(200).json({
+            message: 'Updated product!'
+        })
+
+});
+
+router.delete('/:productId', (req,res,next) => {
+    
+        res.status(200).json({
+            message: 'Deleted product!'
+        })
+
 });
 
 module.exports = router;
