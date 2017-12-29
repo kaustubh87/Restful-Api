aebab2f30a0a72ed951fe50a4fc473e7cc7c2528
kvin1087 <kaustubh87@gmail.com>
2017-12-29 09:41:37 -0600

    Changed connection url

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -7,8 +7,8 @@ const mongoose = require('mongoose');
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
-mongoose.connect("mongodb://kaustubh87:" +process.env.MONGO_ATLAS_PW + "@node-rest-shop-shard-00-00-ddyyz.mongodb.net:27017,node-rest-shop-shard-00-01-ddyyz.mongodb.net:27017,node-rest-shop-shard-00-02-ddyyz.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin", {
-    useMongoClient: true
+mongoose.connect('mongodb://kaustubh87:' + process.env.MONGO_ATLAS_PW + '@node-rest-shop-shard-00-00-ddyyz.mongodb.net:27017,node-rest-shop-shard-00-01-ddyyz.mongodb.net:27017,node-rest-shop-shard-00-02-ddyyz.mongodb.net:27017/test?ssl=true&replicaSet=node-rest-shop-shard-0&authSource=admin',{
+    useMongoClient : true
 });
 
 app.use(morgan('dev'));
