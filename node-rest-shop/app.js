389b7d7b71cd1ef9cfea1fa663bb9c816c193385
kvin1087 <kaustubh87@gmail.com>
2018-01-08 16:35:15 -0600

    Included user routes

--- node-rest-shop/app.js
+++ node-rest-shop/app.js
@@ -6,6 +6,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect(
     "mongodb://kaustubh87:nodeshop@node-shop-shard-00-00-ddyyz.mongodb.net:27017,node-shop-shard-00-01-ddyyz.mongodb.net:27017,node-shop-shard-00-02-ddyyz.mongodb.net:27017/test?ssl=true&replicaSet=node-shop-shard-0&authSource=admin",
@@ -37,6 +38,7 @@ app.use((res, req, next) => {
 
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
