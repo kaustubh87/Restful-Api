2a5a9753832f8d5e96f3ba4e8899b52af5a8506d
kvin1087 <kaustubh87@gmail.com>
2018-01-08 16:35:37 -0600

    Included post user route an bcrypt to hash user password

--- node-rest-shop/api/routes/user.js
+++ node-rest-shop/api/routes/user.js
@@ -6,13 +6,32 @@ const bcrypt = require('bcrypt');
 const User = require('../models/user');
 
 router.post('/signup', (req,res,next) => {
-    const user = new User({
-        _id: new mongoose.Types.ObjectId(),
-        email: req.body.email,
-        password: req.body.password
-    });
+
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        
+        if(err){
+            res.status(500).json({
+                error: err
+            });
+        } else {
+            const user = new User({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
+            });   
+            user.save().then(result => {
+                console.log(result);
+                res.status(201).json({
+                    message: 'User created'
+                })
+            }).catch(err => {
+                console.log(err);
+                res.status(501),json({
+                    error: err
+                });
+            });
+        }
+    });  
 });
-
-
 
 module.exports = router;
