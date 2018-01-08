f55ed80d5c0a7aeef31ab8a6bbb3c2d26c8acbae
kvin1087 <kaustubh87@gmail.com>
2018-01-08 16:00:21 -0600

    Added user routes

---
+++ node-rest-shop/api/routes/user.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+
+const User = require('../models/user');
+
+router.post('/signup', (req,res,next) => {
+
+});
+
+
+
+module.exports = router;
