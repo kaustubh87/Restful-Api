e957fc81c1f5527e4ddcc7161411b8438dabc5aa
kvin1087 <kaustubh87@gmail.com>
2018-01-07 19:40:03 -0600

    Removed console.log

--- node-rest-shop/api/routes/products.js
+++ node-rest-shop/api/routes/products.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'});
+const upload = multer({ dest: 'uploads/' });
 
 const Product = require('../models/product');
 
