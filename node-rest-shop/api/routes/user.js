089cc201258a624cd9de25127180fde1ca8300a8
kvin1087 <kaustubh87@gmail.com>
2018-01-10 16:37:56 -0600

    added json web token sign method

--- node-rest-shop/api/routes/user.js
+++ node-rest-shop/api/routes/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
 
@@ -62,8 +63,20 @@ router.post('/login', (req, res, next) => {
                 });
             }
             if(result) {
+                //User has logged in so generate a token for him
+
+               const token =  jwt.sign({
+                    email: user[0].email,
+                    userId: user[0]._id
+                }, 
+                "secret", 
+                {
+                    expiresIn: "1h"
+                }
+            );
                 return res.status(200).json({
-                    message: 'Auth Successful'
+                    message: 'Auth Successful',
+                    token: token
                 });
             }
             return res.status(401).json({
