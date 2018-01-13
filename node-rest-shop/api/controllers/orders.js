d8bc0b2dbe95fe95e391091117713de33e327dd0
kvin1087 <kaustubh87@gmail.com>
2018-01-13 16:13:26 -0600

    Moved functions into OrdersController

--- node-rest-shop/api/controllers/orders.js
+++ node-rest-shop/api/controllers/orders.js
@@ -1,4 +1,6 @@
 const Order = require('../models/order');
+const Product = require('../models/product');
+const mongoose = require('mongoose');
 
 exports.orders_get_all = (req,res,next) => {
 
@@ -30,3 +32,93 @@ exports.orders_get_all = (req,res,next) => {
     });
 
 }
+
+exports.order_create_order = (req,res,next) => {
+
+    Product.findById(req.body.productId)
+    .then(product => {
+        if(!product){
+            res.status(404).json({
+                message: 'Product not found'
+            })
+        }
+        const order = new Order ({
+            _id: mongoose.Types.ObjectId(),
+            quantity: req.body.quantity,
+            product: req.body.productId
+        });
+        return order.save();
+    })
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Order stored',
+                createdOrder: {
+                    _id: result._id,
+                    product: result.product,
+                    quantity: result.quantity
+                },
+                request: {
+                    type: 'GET',
+                    url: 'http://localhost:3000/orders/' +result._id
+                }
+            });
+        })
+        .catch(err =>{
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+
+}
+
+
+exports.orders_get_order = (req,res,next) => {
+
+       const orderId = req.params.orderId;
+       Order.findById(orderId)
+       .populate('product')
+       .exec()
+       .then(order => {
+           if(!order) {
+               res.status(404).json({
+                   message: 'Order not found'
+               });
+           }
+           res.status(200).json({
+               order: order,
+               request: {
+                   type: 'GET',
+                   url: 'http://localhost:4312/orders'
+               }
+           });
+       })
+       .catch( err =>{
+           res.status(500).json({
+               error : err
+           })
+       });
+   
+};
+
+exports.order_delete_order = (req,res,next) => {
+    const orderId = req.params.orderId;
+    Order.remove({
+        _id: orderId
+    }).exec()
+    .then(data => {
+        res.status(200).json({
+            message: 'Order was deleted successfully',
+            request: {
+                type: 'GET',
+                url: 'http://localhost:4312/orders'
+            }
+        })   
+    })
+    .catch(err => {
+        error: err
+    });
+}
+    
+
