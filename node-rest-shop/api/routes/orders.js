035ae8c8f6696c6ce135ae570bc91889e85e3b39
kvin1087 <kaustubh87@gmail.com>
2018-01-13 16:12:56 -0600

    Removed order functions

--- node-rest-shop/api/routes/orders.js
+++ node-rest-shop/api/routes/orders.js
@@ -1,100 +1,14 @@
 const express = require('express');
 const router = express.Router();
-const mongoose = require('mongoose');
 const checkAuth = require('../middleware/check-auth');
-
-const Order = require('../models/order');
-const Product = require('../models/product');
-
 const OrdersController = require('../controllers/orders');
 
 router.get('/', checkAuth, OrdersController.orders_get_all);
 
-router.post('/', checkAuth, (req,res,next) => {
-   
-    Product.findById(req.body.productId)
-    .then(product => {
-        if(!product){
-            res.status(404).json({
-                message: 'Product not found'
-            })
-        }
-        const order = new Order ({
-            _id: mongoose.Types.ObjectId(),
-            quantity: req.body.quantity,
-            product: req.body.productId
-        });
-        return order.save();
-    })
-        .then(result => {
-            console.log(result);
-            res.status(201).json({
-                message: 'Order stored',
-                createdOrder: {
-                    _id: result._id,
-                    product: result.product,
-                    quantity: result.quantity
-                },
-                request: {
-                    type: 'GET',
-                    url: 'http://localhost:3000/orders/' +result._id
-                }
-            });
-        })
-        .catch(err =>{
-            console.log(err);
-            res.status(500).json({
-                error: err
-            });
-        });
+router.post('/', checkAuth,  OrdersController.order_create_order);
 
-    })
+router.get('/:orderId',checkAuth, OrdersController.orders_get_order);
 
-
-router.get('/:orderId',checkAuth, (req,res,next)=>{
-    const orderId = req.params.orderId;
-   Order.findById(orderId)
-   .populate('product')
-   .exec()
-   .then(order => {
-       if(!order) {
-           res.status(404).json({
-               message: 'Order not found'
-           });
-       }
-       res.status(200).json({
-           order: order,
-           request: {
-               type: 'GET',
-               url: 'http://localhost:4312/orders'
-           }
-       });
-   })
-   .catch( err =>{
-       res.status(500).json({
-           error : err
-       })
-   });
-});
-
-router.delete('/:orderId', checkAuth, (req,res,next)=> {
-    const orderId = req.params.orderId;
-    Order.remove({
-        _id: orderId
-    }).exec()
-    .then(data => {
-        res.status(200).json({
-            message: 'Order was deleted successfully',
-            request: {
-                type: 'GET',
-                url: 'http://localhost:4312/orders'
-            }
-        })   
-    })
-    .catch(err => {
-        error: err
-    })
-});
-
-
+router.delete('/:orderId', checkAuth, OrdersController.order_delete_order);
+    
 module.exports = router;
