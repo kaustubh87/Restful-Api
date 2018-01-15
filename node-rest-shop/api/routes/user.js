1e9d43603e73d0111bcfa12d836a554fe1196c08
kvin1087 <kaustubh87@gmail.com>
2018-01-15 09:56:09 -0600

    Added controller to get all user routes

--- node-rest-shop/api/routes/user.js
+++ node-rest-shop/api/routes/user.js
@@ -5,45 +5,9 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
 const User = require('../models/user');
+const UserController = require('../controllers/users');
 
-router.post('/signup', (req,res,next) => {
-
-    User.find({email: req.body.email}).exec().then(user => {
-        if (user.length >=1 ){
-            return res.status(409).json({
-                message: 'Email address exists'
-            });
-        }
-        else {
-            bcrypt.hash(req.body.password, 10, (err, hash) => {   
-                if(err){
-                    res.status(500).json({
-                        error: err
-                    });
-                } else {
-                    const user = new User({
-                        _id: new mongoose.Types.ObjectId(),
-                        email: req.body.email,
-                        password: hash
-                    });   
-                    user.save().then(result => {
-                        console.log(result);
-                        res.status(201).json({
-                            message: 'User created'
-                        })
-                    }).catch(err => {
-                        console.log(err);
-                        res.status(501),json({
-                            error: err
-                        });
-                    });
-                }
-            });   
-        }
-    });
-
-   
-});
+router.post('/signup', UserController.users_get_all_users);
 
 router.post('/login', (req, res, next) => {
     User.find({
